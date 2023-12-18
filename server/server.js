d95efee56c2a87b1c91430c02d29bc019bdb7189
Sabarna Ghosh Dastidar <73946046+gdsabarna353@users.noreply.github.com>
2023-12-18 09:34:39 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ app.use(passport.session());
 
 app.use(
   cors({
-    origin: "https://keeper-client1.onrender.com",
+    origin: "https://keeper22-frontend.vercel.app",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
@@ -772,7 +772,7 @@ app.get("/auth/login/success", async (req, res) => {
 
 app.get("/auth/logout", (req, res) => {
   req.logout();
-  res.redirect("https://keeper-client1.onrender.com/home");
+  res.redirect("https://keeper22-frontend.vercel.app/home");
 });
 
 app.get(
@@ -783,8 +783,8 @@ app.get(
 app.get(
   "/auth/google/home",
   passport.authenticate("google", {
-    successRedirect: "https://keeper-client1.onrender.com/home",
-    failureRedirect: "https://keeper-client1.onrender.com/login",
+    successRedirect: "https://keeper22-frontend.vercel.app/home",
+    failureRedirect: "https://keeper22-frontend.vercel.app/login",
   })
 );
 
