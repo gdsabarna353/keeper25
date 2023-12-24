892cc418bae231a3b13ee9fbc689b5bc46374880
Sabarna Ghosh Dastidar <73946046+gdsabarna353@users.noreply.github.com>
2023-12-24 16:56:40 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -53,7 +53,7 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://keeper25-frontend.netlify.app');
   next();
 });
 
